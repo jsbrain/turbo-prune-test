14c5750396180005d16984ab015b24c880181519
Manuel Maute <manuel.maute@bradbit.com>
2022-11-15 08:56:53 +0100

    initial commit

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'ui'
+import { Card } from '@pankod/refine-antd'
+
+export default function Web() {
+  return (
+    <div>
+      <h1>Web</h1>
+      <Button />
+      <Card />
+    </div>
+  )
+}
